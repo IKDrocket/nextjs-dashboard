96b212ff76bc74909ba43160c1ec47941246052e
IKDrocket <yusuke1128rocket@gmail.com>
2023-11-25 16:46:05 +0900

    ✨ Customersページを調整

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,26 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import { Metadata } from 'next';
+
+import { Suspense } from 'react';
+import CustomersTable from '@/app/ui/customers/table';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+
+  return (
+    <div className="w-full">
+      <Suspense key={query} fallback={<div />}>
+        <CustomersTable query={query} />
+      </Suspense>
+    </div>
+  );
 }
